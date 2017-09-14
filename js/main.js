13c59a39e15371817ca4f23daa722e7c79bc4faa
Dominika Gnich <d.gnich@wp.pl>
2017-09-14 11:45:28 +0200

    init

--- js/main.js
+++ js/main.js
@@ -1,38 +1,50 @@
 
 
 
-'use strict';
+//'use strict';
+//
+//
+//
+//
+//
+//
+//
+//$(function () {
+    'use strict';
+//    
+//    
+//    
+//    $("a[href*='#']").click(function(){
+//       $('body').animate({
+//           scrollTop: $(this.hash).offset().top - 50}, 500
+//       );
+//    });
+//    
+//    
+//    
+//    
+//    
+//    
+//	$(window).scroll(function () {
+//		if ($(window).scrollTop() >= 50) {
+//			$("#main-nav").addClass("scroll");
+//			$("#main-nav").css({"transition":"all 0.3s"});
+//		} else {
+//			$("#main-nav").removeClass("scroll");
+//		}
+//	});
+//	if ($(window).scrollTop() >= 50) {
+//			$("#main-nav").addClass("scroll");
+//	}
+//});
 
 
 
 
 
 
+    
+    
+    
+ 
 
-$(function () {
-    
-    
-    
-    $("a[href*='#']").click(function(){
-       $('body').animate({
-           scrollTop: $(this.hash).offset().top - 50}, 500
-       );
-    });
-    
-    
-    
-    
-    
-    
-	$(window).scroll(function () {
-		if ($(window).scrollTop() >= 50) {
-			$("#main-nav").addClass("scroll");
-			$("#main-nav").css({"transition":"all 0.3s"});
-		} else {
-			$("#main-nav").removeClass("scroll");
-		}
-	});
-	if ($(window).scrollTop() >= 50) {
-			$("#main-nav").addClass("scroll");
-	}
-});
